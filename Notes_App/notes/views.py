19b6617f715268f9eefeb53cafecfe14f7a1554e
Chitransh <chitranshgaur@outlook.com>
2025-08-11 01:53:56 +0530

    views and urls added in new app

--- Notes_App/notes/views.py
+++ Notes_App/notes/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-# Create your views here.
 
 def home(request):
-    return HttpResponse('<h1>Notes</h1>')
+    return render(request, 'home.html')
+
+def login(request):
+    return render(request, 'login.html')
+
+def register(request):
+    return render(request, 'register.html')
