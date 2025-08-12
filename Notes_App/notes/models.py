2451cb066ec8b666fdf47bbaabaafc7ae82d5112
Chitransh <chitranshgaur@outlook.com>
2025-08-12 23:57:00 +0530

    only user's notes shown when loggedin

--- Notes_App/notes/models.py
+++ Notes_App/notes/models.py
@@ -7,7 +7,7 @@ class Notes(models.Model):
     title=models.CharField(max_length=50)
     content=models.TextField()
     date=models.DateTimeField(default=timezone.now)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='notes')
 
     def __str__(self):
         return self.title
